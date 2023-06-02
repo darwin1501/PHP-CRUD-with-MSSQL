f6ebc8c2aaddb7a36819e108feaa7d2b2309fb20
darwin1501 <darwin.pablo1501@gmail.com>
2023-06-02 11:21:38 +0800

    remove some spacing

--- databases/test_db.php
+++ databases/test_db.php
@@ -1,14 +1,14 @@
 <?php
 function OpenConnection(){
-        $serverName = "localhost,1433";
-        $connectionOptions = array("Database"=>"TestDB",
-            "Uid"=>"sa", "PWD"=>"password123",
-            "TrustServerCertificate"=>true, "Encrypt"=>true
-          );
-        $conn = sqlsrv_connect($serverName, $connectionOptions);
-        if($conn == false)
-            // die(FormatErrors(sqlsrv_errors()));
-            die( print_r( sqlsrv_errors(), true)); 
-        // return $conn;
-        return $conn;
+  $serverName = "localhost,1433";
+  $connectionOptions = array("Database"=>"TestDB",
+      "Uid"=>"sa", "PWD"=>"password123",
+      "TrustServerCertificate"=>true, "Encrypt"=>true
+    );
+  $conn = sqlsrv_connect($serverName, $connectionOptions);
+  if($conn == false)
+      // die(FormatErrors(sqlsrv_errors()));
+      die( print_r( sqlsrv_errors(), true)); 
+  // return $conn;
+  return $conn;
 }
