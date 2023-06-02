e0ce3ce5486e715bd681697a2d94db2870306c54
darwin1501 <darwin.pablo1501@gmail.com>
2023-06-02 11:26:41 +0800

    create add product page

---
+++ views/add_product.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include("./includes/head_content.php") ?>
+    <title>Add Product</title>
+</head>
+<body>
+    <h1>Add product</h1>
+</body>
+</html>
