459052b96f7f9dafcdff0fad20792140b7e5aa8c
darwin1501 <darwin.pablo1501@gmail.com>
2023-06-02 17:20:58 +0800

    add forms to add product

--- views/add_product.php
+++ views/add_product.php
@@ -1,10 +1,38 @@
+<?php 
+include("../model/app_logic.php");
+
+
+if(isset($_POST['submit'])){
+    addProduct();
+    // print_r($_POST);
+    // clear items in post
+}
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <?php include("./includes/head_content.php") ?>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/style.css"/>
     <title>Add Product</title>
 </head>
 <body>
-    <h1>Add product</h1>
+    <main class="flex flex-column align-center justify-center">
+        <div style="margin-top: 50px;">
+            <a class="button" href="../index.php" style="margin-left: -100px;">Go Back.</a>
+            <form method="post" action="add_product.php" class="flex flex-column">
+                <label for="productName">Product Name</label>
+                <input id="productName" name="productName" type="text" required/>
+                <label for="quantity">Quantity</label>
+                <input id="quantity" name="quantity" type="number" style="width: 100px;"/>
+                <input type="submit" value="Add" name="submit" class="button"> 
+            </form>
+        </div>
+    </main>
 </body>
 </html>
