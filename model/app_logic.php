3bcb56c7131c903f25d205101fcff9ebf9f65abb
darwin1501 <darwin.pablo1501@gmail.com>
2023-06-03 11:22:24 +0800

    add update logic

--- model/app_logic.php
+++ model/app_logic.php
@@ -20,7 +20,10 @@ function getProducts(){
                     <td>{$row['id']}</td>
                     <td>{$row['name']}</td>
                     <td>{$row['quantity']}</td>
-                    <td>edit 
+                    <td>
+                        <a href='/PHP-basics/views/edit_product.php?productID={$row['id']}'>
+                            edit
+                        </>
                         <a href='/PHP-basics/?delete={$row['id']}'>
                             delete
                         </>
@@ -36,6 +39,37 @@ function getProducts(){
     {
         echo("Error!");
     }
+}
+
+function findProduct($productID){
+    $conn = OpenConnection();
+    $tsql = "SELECT * FROM Inventory WHERE id = $productID";
+
+    $product = sqlsrv_query($conn, $tsql);
+
+    // print_r($product);
+    $row = sqlsrv_fetch_array($product, SQLSRV_FETCH_ASSOC);
+    
+    return $row;
+}
+
+function updateProduct($productID){
+    // findProduct($_GET['productID']);
+    // echo $productID;
+    $productName = $_POST['productName'];
+    $quantity = $_POST['quantity'];
+    $conn = OpenConnection();
+    $tsql = "UPDATE Inventory SET name = ?, quantity = ? WHERE id = ?";
+    $params = array("$productName", "$quantity", $productID);
+    // $params = array($productName, $quantity);
+    // mysqli_query($con, "UPDATE other_sch set other_sch_name = '$other_sch_name', other_sy = '$other_sy', other_grd = '$other_grd', other_sec = '$other_sec', other_cls_ad = '$other_cls_ad' WHERE `school_id` = '".$_SESSION['school_id']."' ");
+    $updateReview = sqlsrv_prepare($conn, $tsql, $params);
+    $updateResult = sqlsrv_execute($updateReview);
+
+    if ( $updateResult == FALSE)
+        // die(FormatErrors(sqlsrv_errors()));
+        print_r(sqlsrv_errors());
+    sqlsrv_free_stmt($updateReview);
 }
 
 function addProduct(){
